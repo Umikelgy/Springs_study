eda14cde419ac39c6851410dc44a2523f87f0213
10068921 <guangyiliang@alog.com>
2018-08-16 16:20:26 +0800

    spring_cloud

---
+++ SpringCloud/src/main/java/mybac/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package mybac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+ *@author 10068921(LgyTT@alog.com)
+ *@date 2018/8/16 14:04
+ *@description:
+
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
